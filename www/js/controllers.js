8dcd738f1514df0f50961437f4af9da246522d34
Fedor Garin <fedor.garin@gmail.com>
2015-01-26 22:12:16 -0500

    tried adding local storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,12 +2,16 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('IdeasCtrl', function($scope, Ideas) {
+.controller('IdeasCtrl', function($scope, Ideas, Categories) {
   	$scope.ideas = Ideas.all();
 
 	// stringifys date for input
 	$scope.toStringDate = function(date){
 		if (date){return date.yyyymmdd();}
+	}
+
+	$scope.getCategory = function(id){
+		return Categories.get(id);
 	}
 })
 
@@ -31,14 +35,14 @@ angular.module('starter.controllers', [])
 	}
 
 	$scope.saveCategory = function(category){
-		Categories.new(category);
-		$scope.idea.category = category.name;
+		category = Categories.new(category);
+		$scope.idea.category = category.id;
 		$scope.closeModal();
 	}
 
 	// watches category to see if new category modal to be opened
 	$scope.$watch('idea.category', function(value) {
-	    if (value=='New Category') {$scope.openModal()}
+	    if (value==0) {$scope.openModal()} // 'New Category' has value of 0
 	}, true);
 
 	/*
@@ -53,7 +57,8 @@ angular.module('starter.controllers', [])
 
 	$scope.openModal = function() {
 		$scope.category = {};
-		$scope.categoryColors = ['#e5e5e5','#145fd7','#43cee6','#66cc33','#f0b840','#ef4e3a','#8a6de9','#444']
+		$scope.categoryColors = ['#e5e5e5','#145fd7','#43cee6','#66cc33','#f0b840','#ef4e3a','#8a6de9','#444'];
+		$scope.categoryIcons = ['home','search','heart','settings','email','paper-airplane','upload','medkit','map','person-add','chatbox-working','beer','pizza','power','camera','image','flash','bug','music-note','mic-a','bag','card','cash','pricetags','happy','sad','trophy','beaker','earth','planet','bonfire','leaf','model-s','plane','ios7-cart','ios7-home','ios7-bookmarks','ios7-americanfootball', 'ios7-paw', 'ios7-eye'];
 		$scope.modal.show();
 	};
 	$scope.closeModal = function() {
@@ -66,11 +71,10 @@ angular.module('starter.controllers', [])
 	    $scope.category = {};
 	});
 	$scope.setCategoryColor = function(c) {
-		if (!$scope.category){
-			$scope.category = {}
-		}
-
 		$scope.category.color=c;
+	}
+	$scope.setCategoryIcon = function(i) {
+		$scope.category.icon=i;
 	}
 	/*	NEW CATEGORY STUFF */
 })
@@ -79,12 +83,12 @@ angular.module('starter.controllers', [])
   $scope.idea = Ideas.get($stateParams.ideaId);
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('FriendsCtrl', function($scope) {
+
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+.controller('FriendDetailCtrl', function($scope, $stateParams) {
+
 })
 
 .controller('AccountCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,21 +1,44 @@
 angular.module('starter.services', [])
+// angular.module('ionic.utils', [])
 
-.factory('Ideas', function() {
-  // Might use a resource here that returns a JSON array
+// .factory('$localstorage', ['$window', function($window) {
+//   return {
+//     set: function(key, value) {
+//       $window.localStorage[key] = value;
+//     },
+//     get: function(key, defaultValue) {
+//       return $window.localStorage[key] || defaultValue;
+//     },
+//     setObject: function(key, value) {
+//       $window.localStorage[key] = JSON.stringify(value);
+//     },
+//     getObject: function(key) {
+//       return JSON.parse($window.localStorage[key] || '{}');
+//     }
+//   }
+// }]);
 
-  // set up initial empty array of ideas
-  var ideas = [];
+
+
+
+.factory('Ideas', ['$window', function($window) {
+
+  var initialIdeas = [{title: 'My first idea', description: 'something genius', id:1, category: 1}];
+  if (!$window.localStorage['ideas'] || true){$window.localStorage['ideas'] = JSON.stringify(initialIdeas)}
 
   return {
     all: function() {
+      var ideas = JSON.parse($window.localStorage['ideas']);
       return ideas.sort(function(i1,i2){
         return (new Date(i1.date) - new Date(i2.date)) ? -1 : (new Date(i2.date) - new Date(i1.date)) ? 1 : 0;
       });
     },
     remove: function(idea) {
-      ideas.splice(ideas.indexOf(idea), 1);
+      var ideas = JSON.parse($window.localStorage['ideas']);
+      $window.localStorage['ideas'] = JSON.stringify(ideas.splice(ideas.indexOf(idea), 1));
     },
     get: function(ideaId) {
+      var ideas = JSON.parse($window.localStorage['ideas']);
       for (var i = 0; i < ideas.length; i++) {
         if (ideas[i].id === parseInt(ideaId)) {
           return ideas[i];
@@ -24,91 +47,64 @@ angular.module('starter.services', [])
       return null;
     },
     new: function(idea){
-      ideas.unshift(idea);
+      var ideas = JSON.parse($window.localStorage['ideas']);
+      idea.id = getNextId(ideas);
+      $window.localStorage['ideas'] = JSON.stringify(ideas.unshift(idea));
+      return idea;
     },
   }
-})
+}])
 
-.factory('Categories', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Categories', ['$window', function($window) {
 
   // set up initial empty array of ideas
-  var categories = [
-    {name: 'Products'},
-    {name: 'Businesses'},
-    {name: 'Software Projects'},
+  var initialCategories = [
+    {name: ' New Category', id:0},
+    {name: 'Products', icon: 'wand', color: '#43cee6', id:1},
+    {name: 'Businesses', icon: 'social-bitcoin', color: '#ef4e3a', id:2},
+    {name: 'Software Projects', icon: 'code-working', color: '#66cc33', id:3},
   ];
+
+  if (!$window.localStorage['categories'] || true){$window.localStorage['categories'] = JSON.stringify(initialCategories)}
 
   return {
     all: function() {
+      var categories = JSON.parse($window.localStorage['categories']);
       return categories.sort(function(a, b) {return (a['name'] < b['name']) ? 1 : (a['name'] > b['name']) ? -1 : 0});
     },
     remove: function(category) {
-      categories.splice(categories.indexOf(category), 1);
+      var categories = JSON.parse($window.localStorage['categories']);
+      $window.localStorage['categories'] = JSON.stringify(categories.splice(categories.indexOf(category), 1));
     },
     get: function(categoryId) {
+      var categories = JSON.parse($window.localStorage['categories']);
       for (var i = 0; i < categories.length; i++) {
-        if (categories[i].id === parseInt(categoryId)) {
+        if (categories.id === parseInt(categoryId)) {
           return categories[i];
         }
       }
       return null;
     },
     new: function(category){
+      var categories = JSON.parse($window.localStorage['categories']);
       var found = categories.filter(function(item){
         return (item.name==category.name);
       })
       if (!found || !found.length){
-        categories.unshift(category);
+        category.id = getNextId(categories);
+        $window.localStorage['categories'] = JSON.parse(categories.unshift(category));
+        return category;
       }
     },
   }
-})
+}])
 
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  // Some fake testing data
-  var friends = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    notes: 'Enjoys drawing things',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    notes: 'Odd obsession with everything',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlen',
-    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    notes: 'I think he needs to buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    notes: 'Just the nicest guy',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];
-
-
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
+// gets next id from an array of items
+function getNextId(items){
+  if (!items || items.length==0){
+    return 1;
   }
-});
+  var ids = items.map(function(item){return item.id})
+  return (Math.max.apply(null, ids) + 1);
+}
