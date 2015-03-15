fa4f5107b1ca87cad85e3cbf460a3ab5958e9d9f
Fedor Garin <fedor.garin@gmail.com>
2015-03-15 19:07:39 -0400

    port

--- server.js
+++ server.js
@@ -20,5 +20,6 @@
     // app.use(methodOverride());
 
     // listen (start app with node server.js) ======================================
-    app.listen(8080);
+    // app.listen(8080);
+    app.listen(process.env.PORT || 8080)
     console.log("App listening on port 8080");
